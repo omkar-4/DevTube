5387603d665c37df6dbf7fb4c7b751df38b05e7d
omkar-4 <omkar.pro04@gmail.com>
2025-01-03 01:06:09 +0530

    updating with latest working devtube

---
+++ README.md
@@ -0,0 +1,9 @@
+1. Designing the Blueprint - Creating the channel schema for DevTube
+
+- uid is unique id
+- collectionId means when videos will be uploaded on bunny, it will create folder for the channel recognized by collectionId
+- .index = optimizing their indexing for search
+
+2. Structuring DevTube Content : Comment, SUbscription, Tag, and Video Schemas
+
+3.
--- config.js
+++ config.js
@@ -1,60 +1,61 @@
-const passport = require("passport")
-require('dotenv').config() // Load environment variables from .env file
+const passport = require("passport");
+require("dotenv").config(); // Load environment variables from .env file
 
-const GoogleStrategy = require("passport-google-oauth20").Strategy
-const Channel = require("@models/Channel") // Import the Channel model
-const { createUniqueHandle } = require("@lib/utils")
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const Channel = require("@models/Channel"); // Import the Channel model
+const { createUniqueHandle } = require("@lib/utils");
 
 // Configure the Google strategy for use by Passport
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID, // Google client ID from environment variable
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Google client secret from environment variable
-            callbackURL: "/api/auth/google/callback", // Callback URL after Google authentication
-        },
-        async (accessToken, refreshToken, profile, cb) => {
-            try {
-                // Find a channel by email
-                let channel = await Channel.findOne({ email: profile.emails[0].value })
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID, // Google client ID from environment variable
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Google client secret from environment variable
+      callbackURL: "/api/auth/google/callback", // Callback URL after Google authentication
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        // Find a channel by email
+        let channel = await Channel.findOne({ email: profile.emails[0].value });
 
-                // If no channel is found, create a new one
-                if (!channel) {
-
-                    // Find handles with the same base name as the email username and if found make it unique
-                    const handle = await createUniqueHandle(profile.emails[0].value.split('@')[0])
-                    channel = await Channel.create({
-                        name: profile.displayName, // Google profile display name
-                        handle: handle,
-                        email: profile.emails[0].value, // Google profile email
-                        logoURL: profile.photos[0].value.split('=')[0], // Google profile photo URL
-                    })
-                }
-
-                // Return the channel through the callback
-                cb(null, channel)
-            } catch (err) {
-                // Handle errors
-                cb(err)
-            }
+        // If no channel is found, create a new one
+        if (!channel) {
+          // Find handles with the same base name as the email username and if found make it unique
+          const handle = await createUniqueHandle(
+            profile.emails[0].value.split("@")[0]
+          );
+          channel = await Channel.create({
+            name: profile.displayName, // Google profile display name
+            handle: handle,
+            email: profile.emails[0].value, // Google profile email
+            logoURL: profile.photos[0].value.split("=")[0], // Google profile photo URL
+          });
         }
-    )
-)
+
+        // Return the channel through the callback
+        cb(null, channel);
+      } catch (err) {
+        // Handle errors
+        cb(err);
+      }
+    }
+  )
+);
 
 // Serialize the user to decide which data of the user object should be stored in the session
 passport.serializeUser((channel, done) => {
-    done(null, channel.id) // Store the channel id in the session
-})
+  done(null, channel.id); // Store the channel id in the session
+});
 
 // Deserialize the user from the session
 passport.deserializeUser(async (id, done) => {
-    try {
-        // Find the channel by ID
-        const channel = await Channel.findById(id)
-        done(null, channel) // Return the channel
-    } catch (error) {
-        done(error) // Handle errors
-    }
-})
+  try {
+    // Find the channel by ID
+    const channel = await Channel.findById(id);
+    done(null, channel); // Return the channel
+  } catch (error) {
+    done(error); // Handle errors
+  }
+});
 
-module.exports = passport // Export the configured passport
+module.exports = passport; // Export the configured passport
